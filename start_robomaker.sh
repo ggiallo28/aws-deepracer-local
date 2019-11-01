6dbaa4a57bcb82d68fcab44e76e88e154f901fe9
Gianluigi Mucciolo <th3g91@gmail.com>
2019-11-01 18:27:19 +0100

    replace s3 ip address

--- start_robomaker.sh
+++ start_robomaker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 . ./configure_env.sh
-docker run --rm --name dr --env-file ./source/robomaker.env --network sagemaker-local -p 8080:5900 -it crr0004/deepracer_robomaker:console
+sed -e 's/REPLACE/'$MYIP'/g' robomaker.env.dumps > robomaker.env
+docker run --rm --name dr --env-file ./robomaker.env --network sagemaker-local -p 8080:5900 -it crr0004/deepracer_robomaker:console
