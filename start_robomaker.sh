a0c91585ef81c46f92a27ec70ce6bb4ea7486866
Gianluigi Mucciolo <th3g91@gmail.com>
2019-11-01 18:14:47 +0100

    first local run

--- start_robomaker.sh
+++ start_robomaker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 . ./configure_env.sh
-python source/rl_coach/rl_deepracer_coach_robomaker.py
+docker run --rm --name dr --env-file ./source/robomaker.env --network sagemaker-local -p 8080:5900 -it crr0004/deepracer_robomaker:console
